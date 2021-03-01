90ec987cb235dd2721ee45430eda41e704c7aad2
JodhwaniMadhur <jodhwanimadhur@gmail.com>
2021-03-01 12:22:16 +0530

    Second Commit

---
+++ Python Cookbook/Module.py
@@ -0,0 +1,7 @@
+print("---- Marvellous Infosystems by Piyush Khairnar-----")
+
+print("Demonstration of Modules")
+
+import Marvellous
+
+Marvellous.fun()
---
+++ Python Cookbook/Operators.py
@@ -0,0 +1,24 @@
+print("---- Marvellous Infosystems by Piyush Khairnar-----")
+
+print("Demonstration of Operators")
+
+# In Python operators are classified as Arithematic, Assignment, Logical, Relational
+
+# Arithematic operators
+no1 = 10
+no2 = 5
+print(no1+no2)
+print(no1-no2)
+print(no1*no2)
+print(no1/no2)
+print(no1%no2)
+print(no1**no2)
+# Assignment Operators
+a = 10
+b = a
+a = a+5
+a+=5
+x,y = 10,11
+print(x)
+print(y)
+
